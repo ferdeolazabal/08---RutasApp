1ccbdad69812cb8f9da0803b633605f785e48bb9
ferdeolazabal <ferdeolazabal@gmail.com>
2023-05-08 21:04:41 -0300

    First Commit
    - With Icons integrated

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  jsxBracketSameLine: true,
+  printWidth: 100,
+  tabWidth: 4,
+  bracketSpacing: true,
+  bracketSameLine: true,
+};
---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const App = () => {
+    return (
+        <View>
+            <Text>App</Text>
+            <Icon name="list-outline" color="black" size={88} />
+        </View>
+    );
+};
+
+export default App;
