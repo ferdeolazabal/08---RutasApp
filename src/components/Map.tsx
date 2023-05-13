bcfa7e8954e36a126c1e0720149981016421f3ca
ferdeolazabal <ferdeolazabal@gmail.com>
2023-05-13 14:28:01 -0300

    Add a marker to Map component

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 
 const Map = () => {
     return (
         <>
             <MapView
-                provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                provider={PROVIDER_GOOGLE}
                 style={{ flex: 1 }}
                 region={{
                     latitude: -34.603683,
                     longitude: -58.381557,
                     latitudeDelta: 0.015,
                     longitudeDelta: 0.0121,
-                }}
-            />
+                }}>
+                <Marker
+                    coordinate={{
+                        latitude: -34.603683,
+                        longitude: -58.381557,
+                    }}
+                    title="Esto es un titulo"
+                    description="Esta es una descripción!"
+                />
+            </MapView>
         </>
     );
 };
