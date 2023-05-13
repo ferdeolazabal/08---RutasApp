2f56de277f59d17fe2316fa7cc03a353989b1239
ferdeolazabal <ferdeolazabal@gmail.com>
2023-05-13 14:03:26 -0300

    Correct styles to be more responsible

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 
 const MapScreen = () => {
     return (
-        <View style={StyleSheet.absoluteFillObject}>
+        <View style={{ flex: 1 }}>
             <MapView
                 provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-                style={StyleSheet.absoluteFillObject}
+                style={{ flex: 1 }}
                 region={{
                     latitude: -34.603683,
                     longitude: -58.381557,
