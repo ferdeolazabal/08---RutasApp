6acab5b8fc15c09c1c7fa1f1ba813979ddcc5755
ferdeolazabal <ferdeolazabal@gmail.com>
2023-05-13 13:09:36 -0300

    Apply google maps integration
    - Add new library

--- App.tsx
+++ App.tsx
@@ -4,6 +4,9 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Navigator } from './src/navigator/navigator';
 import { PermissionsProvider } from './src/context/PermissionsContext';
+import { enableLatestRenderer } from 'react-native-maps';
+
+enableLatestRenderer();
 
 const AppState = ({ children }: any) => {
     return <PermissionsProvider>{children}</PermissionsProvider>;
--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,12 +1,34 @@
-import { View, Text } from 'react-native';
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 
 const MapScreen = () => {
     return (
-        <View>
-            <Text style={{ color: 'black' }}>MapScreen</Text>
+        <View style={styles.container}>
+            <MapView
+                provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                style={styles.map}
+                region={{
+                    latitude: -34.603683,
+                    longitude: -58.381557,
+                    latitudeDelta: 0.015,
+                    longitudeDelta: 0.0121,
+                }}></MapView>
         </View>
     );
 };
 
 export default MapScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        // height: 400,
+        // width: 400,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
+});
