83423cf11596cc9cff46b9e7691b9abded6fb1da
ferdeolazabal <ferdeolazabal@gmail.com>
2023-05-13 14:01:23 -0300

    Change styles into inline styles

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -4,31 +4,19 @@ import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER
 
 const MapScreen = () => {
     return (
-        <View style={styles.container}>
+        <View style={StyleSheet.absoluteFillObject}>
             <MapView
                 provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-                style={styles.map}
+                style={StyleSheet.absoluteFillObject}
                 region={{
                     latitude: -34.603683,
                     longitude: -58.381557,
                     latitudeDelta: 0.015,
                     longitudeDelta: 0.0121,
-                }}></MapView>
+                }}
+            />
         </View>
     );
 };
 
 export default MapScreen;
-
-const styles = StyleSheet.create({
-    container: {
-        ...StyleSheet.absoluteFillObject,
-        // height: 400,
-        // width: 400,
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-    },
-    map: {
-        ...StyleSheet.absoluteFillObject,
-    },
-});
